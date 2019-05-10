8832742f119941a6f158a60db77f5e1b8f81ede2
Samarialla <sergojavier23@gmail.com>
2019-05-10 16:11:39 -0400

    10-05-2019

---
+++ application/views/turismo.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Turismo</title>
+		<!--	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/estilos.css">-->
+
+    <!--materialize-->
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/materialize.min.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/materialize.css">
+	    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/turismo.css">
+
+    <script src="<?php echo base_url() ?>assets/js/materialize.js"></script>
+      <script src="<?php echo base_url() ?>assets/js/materialize.min.js"></script>
+
+			<!--fontawesome-->
+			<link rel="stylesheet" type="text/css" href="assets/css/all.css">
+    	<script src="<?php echo base_url(); ?>assets/js/all.js"></script>
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/all.min.css">
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+	    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.1.1.min.js"></script>
+
+
+  </head>
+  <body id="body" class="container">
+
+  </body>
+</html>
