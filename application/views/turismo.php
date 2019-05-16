23039a1f92ee4cba8283c0416bb82a7133caf74f
Samarialla <sergojavier23@gmail.com>
2019-05-16 16:01:47 -0400

    16/05/2019

--- application/views/turismo.php
+++ application/views/turismo.php
@@ -31,6 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <div class="row">
         <div class="col s2">
           <img id="logo" src="<?php echo base_url(); ?>assets/images/logo.png" class="responsive-img"/>
+
         </div>
         <div class="col s6 offset-s4">
           <a id="boton" class="waves-effect waves-light btn-small"><i class="fas fa-home"></i>Inicio</a>
@@ -38,10 +39,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <a id="boton" class="waves-effect waves-light btn-small"><i class="fas fa-place-of-worship"></i>Mejores Viajes</a>
           <a id="boton" class="waves-effect waves-light btn-small"><i class="fab fa-angellist"></i>Recomentaciones</a>
 
-
         </div>
+          <div id="letra" class="col s6">
+            <h4>Creando mi propio camino</h4>
+         </div>
+      </div>
+      <div class="row">
+
       </div>
     </header>
+    <div class="row">
+    <div class="col s1">
+      <ul>
+      <li ><i id="icon" class="fab fa-instagram"></i></li>
+      <li><i id="icon" class="fab fa-facebook-square"></i></li>
+      <li><i id="icon" class="fab fa-twitter"></i></li>
+      <li><i id="icon" class="fab fa-youtube"></i></li>
+      <li><i id="icon" class="fas fa-camera-retro"></i></li>
+    </ul>
+    </div>
 
+    <div class="col s3">
+      <img id="image1" src="<?php echo base_url(); ?>assets/images/buceo.jpg" class="responsive-img"/>
+      <img id="image2" src="<?php echo base_url(); ?>assets/images/calles.jpg" class="responsive-img"/>
+    </div>
+    <div class="col s4">
+      <img id="image3" src="<?php echo base_url(); ?>assets/images/iglesia.jpg" class="responsive-img"/>
+    </div>
+    <div class="col s3">
+      <img id="image4" src="<?php echo base_url(); ?>assets/images/egipto.jpg" class="responsive-img"/>
+      <img id="image4" src="<?php echo base_url(); ?>assets/images/iglesia.jpg" class="responsive-img"/>
+      <img id="image4" src="<?php echo base_url(); ?>assets/images/calles.jpg" class="responsive-img"/>
+    </div>
+
+  </div>
+    <div class="row">
+      <div class="col s12">
+      <div>
+    </div>
   </body>
 </html>
